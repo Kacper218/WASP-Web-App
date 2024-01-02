db4a60354a9fcc7756e72dea8cfe62b24d31dacd
Kacper Przybyszewski <308196@o365.stud.umk.pl>
2024-01-02 15:54:29 +0100

    Added post request

---
+++ WASP_Web_App/ApiClient.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WASP_Web_App.Entities;
+
+namespace WASP_Web_App
+{
+    public class ApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiClient()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7155"); // Replace with your API base URL
+        }
+
+        public async Task<string> GetLoginInfo()
+        {
+            try
+            {
+                var firstAuth = new Auth
+                {
+                    Login = "Karol",
+                    Password = "admin1",
+                    User_ID = 1
+                };
+            
+                JsonContent content = JsonContent.Create(firstAuth);
+
+                var response = await _httpClient.PostAsync("/Auth", content);
+            
+
+               if(response.IsSuccessStatusCode)
+                { 
+                   
+                    var responseData = await response.Content.ReadAsStringAsync();               
+
+                    Console.WriteLine(responseData);
+
+                    return responseData;
+
+                }
+                return "Nie udało się zalogować";
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return "Blad podczas Pobierania API";
+            }
+        }
+    }
+}
--- WASP_Web_App/Controllers/HomeController.cs
+++ WASP_Web_App/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WASP_Web_App.Entities;
 using WASP_Web_App.Models;
 
 namespace WASP_Web_App.Controllers
@@ -7,17 +8,37 @@ namespace WASP_Web_App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApiClient _apiClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApiClient apiClient)
         {
             _logger = logger;
+            _apiClient = apiClient;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
-        }
+            try
+            {
 
+                string authData = await _apiClient.GetLoginInfo();
+               
+                _logger.LogInformation("Karol git: {AuthData}", authData);
+
+                // Process weatherData or pass it to the view
+
+                ViewData["AuthData"] = authData;
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Blad fetchowania");
+                return View();
+            }
+
+        }
         public IActionResult Privacy()
         {
             return View();
